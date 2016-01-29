75e62eac2fb6faa813110a3571eebe13c2a8c476
Sean Donovan <sdonovan@gatech.edu>
2016-01-29 09:06:24 -0500

    First pass at Connection and ConnectionManager. Not tested.

--- shared/Connection.py
+++ shared/Connection.py
@@ -1,6 +1,16 @@
 # Copyright 2016 - Sean Donovan
 # AtlanticWave/SDX Project
 
+import socket
+import cPickle as Pickle
+import logging
+import threading
+import sys
+import struct
+
+class ConnectionError(Exception):
+    pass
+
 
 class Connection(object):
     ''' Handed out by the ConnectionManager. It’s basically the same as a 
@@ -8,32 +18,101 @@ class Connection(object):
         it’ll be a socket, but in the future, this can be changed to be a TLS 
         socket, for instance. '''
 
-    def __init__(self):
-        pass
+    def __init__(self, address, port, sock):
+        # Setup logging
+
+        # Allocate resources/init variables
+        self.address = address
+        self.port = port
+        self.sock = sock
+
+        self.recv_cb = None
+        self.recv_thread = None
+        
+    def __del__(self):
+        # Destructor
+        self.close()
 
     def recv(self):
-        pass
+        ''' Receives an item. This is a blocking call. '''
+        try:
+            # Based on https://code.activestate.com/recipes/408859-socketrecv-three-ways-to-turn-it-into-recvall/
+            total_len = 0
+            total_data = []
+            size = sys.maxint
+            size_data = ''
+            sock_data = ''
+            recv_size = 8192
+            while total_len < size:
+                sock_data = self.sock.recv(recv_size)
+                if not total_data:
+                    if len(sock_data) > 4:
+                        size_data += sock_data
+                        size = struct.unpack('>i', size_data[:4])[0]
+                        recv_size=size
+                        if recv_size>524288 :
+                            recv_size = 524288
+                        total_data.append(size_data[4:])
+                    else:
+                        size_data += sock_data
+                else:
+                    total_data.append(sock_data)
+                total_len = sum([len(i) for i in total_data])
+            data_raw = ''.join(total_data)
 
-    def send(self):
+            # Unpickle!
+            data = pickle.loads(data_raw)
+            return data
+
+        except:
+            raise
+
+
+    def send(self, data):
+        ''' Sends an item. '''
+        try:
+            # Based on https://code.activestate.com/recipes/408859-socketrecv-three-ways-to-turn-it-into-recvall/
+            data_raw = pickle.dumps(data)
+            self.sock.sendall(struct.pack('>i', len(data_raw))+data_raw)
+        except:
+            raise
         pass
 
     def close(self):
-        pass
+        ''' Close out the connection. '''
+        try:
+            self.sock.close()
+        except:
+            raise
 
     def register_receive_callback(self, callback):
         ''' If using non-blocking receive, this saves the callback that receives
             data. 
             callback takes one item as its input (any type it chooses). '''
-        pass
+        self.recv_cb = callback
 
     def start_receive_thread(self):
         ''' This starts a separate thread for receiving data. It will call back
             the callback set in register_receive_callback(). '''
-        pass
+        if self.recv_cb == None:
+            raise ConnectionError("recv_cb not filled in")
+        self.recv_thread = threading.Thread(target=self._receive_thread)
+        self.recv_thread.daemon = True
+        self.recv_thread.start()
 
     def _receive_thread(self):
         ''' The actual thread that receives items. Private. '''
-        pass
+        try:
+            while True:
+                data = self.recv()
+                self.recv_cb(data)
+        except Error as e:
+            # Likely connection going down.
+            pass
+        
+        finally:
+            # Clean up connectiong
+            self.close()
 
     
 
--- shared/ConnectionManager.py
+++ shared/ConnectionManager.py
@@ -3,6 +3,12 @@
 
 
 from shared.Singleton import Singleton
+from shared.Connection import Connection
+
+import socket
+import logging
+from threading import Thread
+
 
 class ConnectionManager(object):
     ''' This is a parent class for handling connections, dispatching the new 
@@ -18,20 +24,45 @@ class ConnectionManager(object):
         ''' Register for a new connection callback. When a new connection comes 
             in, handling_function will be called.
             handling_function should take a Connection as its input. '''
-        pass
+        self.listening_callback = handling_function
     
     def open_listening_port(self, ip, port):
-        ''' Opens a listening port. '''
-        pass
+        ''' Opens a listening port. This is a blocking call.'''
+        self.listening_address = ip
+        self.listening_port = port
+        self.listening_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        try:
+            self.listening_sock.bind((self.listening_address,
+                                      self.listening_port))
+            self.listening_sock.listen(1)
+            while True:
+                client_sock, client_address = self.listening_sock.accept()
+                new_cxn_thread = Thread(target=self._internal_new_connection,
+                                        args=(client_sock, client_address))
+                new_cxn_thread.daemon = True
+                new_cxn_thread.start()
+
+        except:
+            raise
     
-    def _internal_new_connection(self):
+    def _internal_new_connection(self, sock, address):
         ''' This will call the callback set in new_connection_callback(). Since
             the ConnectionManager handles the connection, it needs to set up 
             tracking of the connection and the like. Creates the Connection, and
             passes this to the saved new connection handling function. 
             Private. '''
-        pass
-    def open_outbound_connection(self):
+        client_ip, client_port = address
+        client_connection = Connection(client_ip, client_port, sock)
+        self.listening_callback(client_connection)
+        
+    def open_outbound_connection(self, ip, port):
         ''' This opens an outbound connection to a given address. Returns a 
             Connection that can be used for sending or receiving. '''
-        pass
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        try:
+            self.sock.connect((self.address, self.port))
+        except:
+            raise
+        return Connection(ip, port, sock)
