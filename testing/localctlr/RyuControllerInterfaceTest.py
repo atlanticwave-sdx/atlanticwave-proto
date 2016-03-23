537cc7c4a6f11795f67b41109c2e9577485d844c
Sean Donovan <sdonovan@gatech.edu>
2016-03-23 13:38:25 -0400

    Before totally reworking how the different localctlr modules talk to each other. I'm having issues with Queues not having information pulled out at the far end. Not sure why. Going to rework to be based on Connection/ConnectionManager instead.

--- testing/localctlr/RyuControllerInterfaceTest.py
+++ testing/localctlr/RyuControllerInterfaceTest.py
@@ -47,10 +47,11 @@ class RyuControllerFullTests(unittest.TestCase):
         cls.trans = cp.TranslateInterface
 
         # Setup the virtual switch
-        subprocess.call(['ovs-vsctl', 'add-br', 'br_ovs'])
-        subprocess.call(['ovs-vsctl', 'add-port', 'br_ovs', 'vi0', '--', 'set', 'Interface', 'vi0', 'type=internal'])
-        subprocess.call(['ovs-vsctl', 'set', 'bridge', 'br_ovs', 'other-config:datapath-id=0000000000000001'])
-        subprocess.call(['ovs-vsctl', 'set-controller', 'br_ovs', 'tcp:127.0.0.1:6633'])
+        subprocess.check_call(['mn', '-c'])
+        subprocess.check_call(['ovs-vsctl', 'add-br', 'br_ovs'])
+        subprocess.check_call(['ovs-vsctl', 'add-port', 'br_ovs', 'vi0', '--', 'set', 'Interface', 'vi0', 'type=internal'])
+        subprocess.check_call(['ovs-vsctl', 'set', 'bridge', 'br_ovs', 'other-config:datapath-id=0000000000000001'])
+        subprocess.check_call(['ovs-vsctl', 'set-controller', 'br_ovs', 'tcp:127.0.0.1:6633'])
 
 
         # Wait for switch to connect to controller
@@ -64,7 +65,10 @@ class RyuControllerFullTests(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         # Delete virtual switch
-        subprocess.call(['ovs-vsctl', 'del-br', 'br_ovs'])
+        # FIXME: whenever this is called, there is an error: "cannot switch to a different thread"
+        #subprocess.check_call(['ovs-vsctl', 'del-br', 'br_ovs'])
+        #sleep(5)
+        print "end of tearDownClass"
 
     def test_rule_installation(self):
         match = OpenFlowMatch([ETH_DST("00:00:00:00:00:01")])
@@ -74,8 +78,9 @@ class RyuControllerFullTests(unittest.TestCase):
 
         self.ctlrint.send_command(ofr)
 
-        output = subprocess.check_output(['ovs-ofctl', 'dump-flows', 'br_ovs'])
-        print output
+        #output = subprocess.check_output(['ovs-ofctl', 'dump-flows', 'br_ovs'])
+        #print output
+        print "End of test_rule_installation"
         
 
 if __name__ == '__main__':
