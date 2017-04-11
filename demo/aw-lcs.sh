dce3cfd9505e0d44a5e269ba5b6eeacc7c4e60fd
Sean Donovan <sdonovan1985@users.noreply.github.com>
2017-04-11 10:12:39 -0400

    Corsa bandwidth management integration (#23)
    * Code changes for bandwidth management. Not Tested. Needs dockerfile updates.
    
    * Bugs found while testing in VM. VM appears to be working the same as before.
    
    * Update to *hopefully* correct VLAN rate limiting.
    
    * Rate limiting bridge information.
    
    * Manifest file fix for rate limiter bridge entry.
    
    * Token wasn't being referenced correctly.
    
    * Diagnostics.
    
    * Changing URL for Tunnels.
    
    * Diagnostics.
    
    * Diagnostics.
    
    * Diagnostics.
    
    * Diagnostics.
    
    * Diagnostics.
    
    * Diagnostics.
    
    * Diagnostics.
    
    * Diagnostics.
    
    * Diagnostics.
    
    * Diagnostics.
    
    * Diagnostics.
    
    * Diagnostics.
    
    * Diagnostics.
    
    * Diagnostics.
    
    * Diagnostics.
    
    * Diagnostics.
    
    * Diagnostics.
    
    * Diagnostics.
    
    * Updates to dockerfiles, post manual testing. Place manifest files that should be used in the docker/ directory.

--- demo/aw-lcs.sh
+++ demo/aw-lcs.sh
@@ -4,9 +4,9 @@ foo=""
 #foo+=($tab -e "bash -c 'cd ../localctlr; python LocalController.py ac ../demo/rnp-topo.manifest';bash")
 
 
-foo+=($tab -e "bash -c 'cd ../localctlr; python LocalController.py atl ../demo/aw-three-site.manifest';bash")
-foo+=($tab -e "bash -c 'cd ../localctlr; python LocalController.py mia ../demo/aw-three-site.manifest';bash")
-foo+=($tab -e "bash -c 'cd ../localctlr; python LocalController.py gru ../demo/aw-three-site.manifest';bash")
+foo+=($tab -e "bash -c 'cd ../localctlr; python LocalController.py -n atl -m ../demo/aw-three-site.manifest';bash")
+foo+=($tab -e "bash -c 'cd ../localctlr; python LocalController.py -n mia -m ../demo/aw-three-site.manifest';bash")
+foo+=($tab -e "bash -c 'cd ../localctlr; python LocalController.py -n gru -m ../demo/aw-three-site.manifest';bash")
 #echo "$foo"
 
 gnome-terminal "${foo[@]}"
