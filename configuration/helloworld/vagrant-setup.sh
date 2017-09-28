f7f5c68a016b44e6c7499ded68e7a10f7c840b4b
Sean Donovan <sdonovan@gatech.edu>
2017-09-28 13:27:53 -0400

    Vagrant testing updates.

--- configuration/helloworld/vagrant-setup.sh
+++ configuration/helloworld/vagrant-setup.sh
@@ -5,7 +5,6 @@ sudo apt update
 sudo apt install -y git mininet docker.io
 
 #sudo groupadd docker #Already added
-echo $USER
 sudo usermod -aG docker $USER
 
 
@@ -14,17 +13,16 @@ git clone https://github.com/atlanticwave-sdx/atlanticwave-proto.git
 
 # Docker work: build SDX Controller and Local Controller containers
 cd ~/atlanticwave-proto/
-pwd
-pwd
-pwd
 cp configuration/helloworld/helloworld.manifest docker/sdx_container/
 cp configuration/helloworld/helloworld.manifest docker/lc_container/
 
+sudo service docker restart
+
 cd docker/sdx_container
-docker build -t sdx_container .
+sudo docker build -t sdx_container .
 rm helloworld.manifest
 
 cd ../lc_container
-docker build -t lc_container .
+sudo docker build -t lc_container .
 rm helloworld.manifest
 
