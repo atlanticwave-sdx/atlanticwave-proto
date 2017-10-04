dd58139e7fd6ff2ce4472ce638b6e58148fc5cf9
Sean Donovan <sdonovan@gatech.edu>
2017-10-04 15:56:19 -0400

    Making easier to use. Allow users to start everything from ~

--- configuration/helloworld/3-start-mininet.sh
+++ configuration/helloworld/3-start-mininet.sh
@@ -3,4 +3,5 @@
 echo "Type 'help' for details on how to use Mininet"
 echo "There are four nodes: atl, atldtn, mia, miadtn"
 # Start Mininet!
+cd atlanticwave-proto/configuration/helloworld/
 sudo python helloworld-mn-topo.py
