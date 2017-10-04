b7ddd72aa9431d9686d69dc310733e0eb218bded
Sean Donovan <sdonovan@gatech.edu>
2017-10-04 13:03:59 -0400

    Fixing issues with port routing

--- configuration/helloworld/2-start-lcctlr.sh
+++ configuration/helloworld/2-start-lcctlr.sh
@@ -3,7 +3,5 @@
 #
 cd ../../localctlr/
 docker rm lchelloworld
-SDXIPVAL="$(ifconfig | awk '/inet addr/{print substr($2,6)}' | awk 'NR==1{print $1}')"
-docker run -e MANIFEST="/helloworld.manifest" -e SITE="br1" -e SDXIP=$SDXIPVALl -p 6680:6680 -it --name=lchelloworld lc_container
-
-
+SDXIPVAL="172.17.0.2"
+docker run -e MANIFEST="/helloworld.manifest" -e SITE="br1" -e SDXIP=$SDXIPVAL -p 6680:6680 -it --name=lchelloworld lc_container
