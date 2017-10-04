d2e6ccb5ce5d5e6233fa762a9ff4b9961b4a147f
Sean Donovan <sdonovan@gatech.edu>
2017-10-04 10:29:19 -0400

    Adding a more specific IP address to try.

--- configuration/helloworld/2-start-lcctlr.sh
+++ configuration/helloworld/2-start-lcctlr.sh
@@ -3,6 +3,7 @@
 #
 cd ../../localctlr/
 docker rm lchelloworld
-docker run -e MANIFEST="/helloworld.manifest" -e SITE="br1" -e SDXIP="127.0.0.1" -p 6680:6680 -it --name=lchelloworld lc_container
+SDXIPVAL="$(ifconfig | awk '/inet addr/{print substr($2,6)}' | awk 'NR==1{print $1}')"
+docker run -e MANIFEST="/helloworld.manifest" -e SITE="br1" -e SDXIP=$SDXIPVALl -p 6680:6680 -it --name=lchelloworld lc_container
 
 
