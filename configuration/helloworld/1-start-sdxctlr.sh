d4ccc3d567d7529643108331f65af4991814ec3d
Sean Donovan <sdonovan@gatech.edu>
2017-10-04 13:15:31 -0400

    SDX Controller needs both internal and external access.

--- configuration/helloworld/1-start-sdxctlr.sh
+++ configuration/helloworld/1-start-sdxctlr.sh
@@ -3,4 +3,4 @@
 #
 cd ../../sdxctlr/
 docker rm sdxhelloworld
-docker run -e MANIFEST="/helloworld.manifest" -e IPADDR="172.17.0.2" -e PORT="5000" -e LCPORT="5555" -p 5000:5000 -it --name=sdxhelloworld sdx_container
+docker run -e MANIFEST="/helloworld.manifest" -e IPADDR="0.0.0.0" -e PORT="5000" -e LCPORT="5555" -p 5000:5000 -it --name=sdxhelloworld sdx_container
