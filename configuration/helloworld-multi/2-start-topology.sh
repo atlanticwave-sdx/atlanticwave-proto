8ce71564a830655189788e4186431aa670e904a6
Sean Donovan <sdonovan@gatech.edu>
2018-01-04 07:53:15 -0500

    #45 - Update permissions, change a path in 2-start-topology.sh

--- configuration/helloworld-multi/2-start-topology.sh
+++ configuration/helloworld-multi/2-start-topology.sh
@@ -3,5 +3,5 @@
 echo "Type 'help' for details on how to use Mininet"
 echo "There are four nodes: atl, atldtn, mia, miadtn"
 # Start Mininet!
-cd atlanticwave-proto/configuration-multi/helloworld/
+cd atlanticwave-proto/configuration-multi/helloworld-multi/
 sudo python helloworld-mn-topo.py
