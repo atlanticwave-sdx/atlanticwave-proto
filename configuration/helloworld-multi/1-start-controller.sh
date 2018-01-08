f4d1460407b592b556d699af4b8ab4ec34e722f9
Sean Donovan <sdonovan@gatech.edu>
2018-01-08 12:42:34 -0500

    #45 - Bug fixes related to LC-SDX controller communication.

--- configuration/helloworld-multi/1-start-controller.sh
+++ configuration/helloworld-multi/1-start-controller.sh
@@ -13,7 +13,7 @@ cd ../localctlr/
 docker rm westctlr
 docker rm centctlr
 docker rm eastctlr
-SDXIPVAL="172.17.0.2"
+export SDXIPVAL="172.17.0.2"
 docker run -e MANIFEST="/helloworld.manifest" -e SITE="westctlr" -e SDXIP=$SDXIPVAL -p 6680:6680 -dit --name=westctlr lc_container
 docker run -e MANIFEST="/helloworld.manifest" -e SITE="centctlr" -e SDXIP=$SDXIPVAL -p 6681:6681 -dit --name=centctlr lc_container
 docker run -e MANIFEST="/helloworld.manifest" -e SITE="eastctlr" -e SDXIP=$SDXIPVAL -p 6682:6682 -dit --name=eastctlr lc_container
