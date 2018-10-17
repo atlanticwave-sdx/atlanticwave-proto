d88ea51c524fded7ced879bdde544e0f0b4ea406
Sean Donovan <sdonovan@gatech.edu>
2018-10-17 12:02:57 -0400

    #93 - Testing cancel()/clear(), fixed a few issues with put that cropped up.

--- testing/sdxctlr/SenseAPITest.py
+++ testing/sdxctlr/SenseAPITest.py
@@ -61,7 +61,7 @@ class PutGetDeltaTest(unittest.TestCase):
         #print api._get_all_deltas()
         delta_id = 1
         api._put_delta(delta_id, self.raw_request, self.sdx_rule,
-                       self.model_id, self.status)
+                       self.model_id)
         
     def test_put_get(self):
         # Make sure _put_delta() and _get_delta_by_id() work
@@ -74,14 +74,14 @@ class PutGetDeltaTest(unittest.TestCase):
 
         delta_id = 2
         api._put_delta(delta_id, self.raw_request, self.sdx_rule,
-                       self.model_id, self.status)
+                       self.model_id)
         raw_delta = api._get_delta_by_id(delta_id)
 
         self.failUnlessEqual(raw_delta['delta_id'], delta_id)
         self.failUnlessEqual(raw_delta['raw_request'], self.raw_request)
         self.failUnlessEqual(raw_delta['sdx_rule'], self.sdx_rule)
         self.failUnlessEqual(raw_delta['model_id'], self.model_id)
-        self.failUnlessEqual(raw_delta['status'], self.status)
+        self.failUnlessEqual(raw_delta['status'], STATUS_CREATED)
 
     def test_get(self):
         # Test _put_delta() and get_delta()
@@ -150,8 +150,8 @@ class PutGetDeltaTest(unittest.TestCase):
         #   - Put invalid things
         delta_id = 6
         self.failUnlessRaises(SenseAPIError, api._put_delta,
-                              delta_id, self.raw_request, self.sdx_rule,
-                              self.model_id) # missing one item            
+                              delta_id, self.raw_request, self.sdx_rule)
+                              # missing one item            
         
         #   - Put duplicate delta
         delta_id = 7
@@ -172,8 +172,7 @@ class PutGetDeltaTest(unittest.TestCase):
                        self.model_id, self.status)
         self.failUnlessRaises(SenseAPIError, api._put_delta,
                               delta_id, update=True)
-        
-        pass
+
 
     def test_get_by_id_invalid(self):
         # Test invalid _get_delta_by_id() commands
@@ -274,13 +273,123 @@ class CommitTest(unittest.TestCase):
 
         delta_id = 20
         api._put_delta(delta_id, self.raw_request, self.sdx_rule,
-                       self.model_id, self.status)
+                       self.model_id)
         api.commit(delta_id)
 
         
 
-        
-                    
+class CancelTest(unittest.TestCase):
+    def setUp(self):
+        self.raw_request = "raw_request"
+        self.model_id  = 3
+        self.status_good = STATUS_GOOD
+        self.status_created = STATUS_CREATED
 
+        
+        self.starttime = "1985-04-12T12:23:56"
+        self.endtime = "2985-04-12T12:23:56"
+        self.srcswitch = "br1"
+        self.dstswitch = "br2"
+        self.srcport = 1
+        self.dstport = 2
+        self.srcvlan = 100
+        self.dstvlan = 200
+        self.bandwidth = 100000
+        self.srcendpoint = "atldtn-br1"
+        self.dstendpoint = "miadtn-br2"
+        
+        jsonrule = {"L2Tunnel":{
+            "starttime":self.starttime,
+            "endtime":self.endtime,
+            "srcswitch":self.srcswitch,
+            "dstswitch":self.dstswitch,
+            "srcport":self.srcport,
+            "dstport":self.dstport,
+            "srcvlan":self.srcvlan,
+            "dstvlan":self.dstvlan,
+            "bandwidth":self.bandwidth}}
+        self.sdx_rule = L2TunnelPolicy("SENSE", jsonrule)   
+
+    def test_basic_committed_cancel(self):
+        # Calling cancel() on committed deltaid, expect STATUS_GOOD back
+        tm = TopologyManager(topology_file=BASIC_MANIFEST_FILE)
+        rm = RuleManager(DB_FILE,
+                         send_user_rule_breakdown_add=add_rule,
+                         send_user_rule_breakdown_remove=rm_rule)
+
+        api = SenseAPI(DB_FILE)
+
+        delta_id = 30
+        api._put_delta(delta_id, self.raw_request, self.sdx_rule,
+                       self.model_id)
+        api.commit(delta_id)
+
+        self.failUnlessEqual(STATUS_GOOD, api.cancel(delta_id))
+        self.failUnlessEqual(None, api._get_delta_by_id(delta_id))
+
+    def test_basic_committed_clear(self):
+        # Calling clear() on committed deltaid, expect STATUS_GOOD back
+        tm = TopologyManager(topology_file=BASIC_MANIFEST_FILE)
+        rm = RuleManager(DB_FILE,
+                         send_user_rule_breakdown_add=add_rule,
+                         send_user_rule_breakdown_remove=rm_rule)
+
+        api = SenseAPI(DB_FILE)
+
+        delta_id = 31
+        api._put_delta(delta_id, self.raw_request, self.sdx_rule,
+                       self.model_id)
+        api.commit(delta_id)
+
+        self.failUnlessEqual(STATUS_GOOD, api.clear(delta_id))
+        self.failUnlessEqual(None, api._get_delta_by_id(delta_id))
+
+    def test_basic_uncommitted_cancel(self):
+        # Calling cancel() on uncommitted deltaid, expect STATUS_GOOD back
+        tm = TopologyManager(topology_file=BASIC_MANIFEST_FILE)
+        rm = RuleManager(DB_FILE,
+                         send_user_rule_breakdown_add=add_rule,
+                         send_user_rule_breakdown_remove=rm_rule)
+
+        api = SenseAPI(DB_FILE)
+
+        delta_id = 32
+        api._put_delta(delta_id, self.raw_request, self.sdx_rule,
+                       self.model_id)
+
+        self.failUnlessEqual(STATUS_GOOD, api.clear(delta_id))
+        self.failUnlessEqual(None, api._get_delta_by_id(delta_id))
+    
+    def test_cancel_bad_delta(self):
+        # Calling cancel() on bad delta, expect STATUS_NOT_FOUND back
+        tm = TopologyManager(topology_file=BASIC_MANIFEST_FILE)
+        rm = RuleManager(DB_FILE,
+                         send_user_rule_breakdown_add=add_rule,
+                         send_user_rule_breakdown_remove=rm_rule)
+
+        api = SenseAPI(DB_FILE)
+
+        delta_id = 33
+
+        self.failUnlessEqual(STATUS_NOT_FOUND, api.cancel(delta_id))
+
+    def test_double_clear(self):
+        # Calling cancel() first time, expect STATUS_GOOD back
+        # Calling cancel() on same deltaid, expect STATUS_NOT_FOUND back
+        tm = TopologyManager(topology_file=BASIC_MANIFEST_FILE)
+        rm = RuleManager(DB_FILE,
+                         send_user_rule_breakdown_add=add_rule,
+                         send_user_rule_breakdown_remove=rm_rule)
+
+        api = SenseAPI(DB_FILE)
+
+        delta_id = 34
+        api._put_delta(delta_id, self.raw_request, self.sdx_rule,
+                       self.model_id)
+
+        self.failUnlessEqual(STATUS_GOOD, api.cancel(delta_id))
+        self.failUnlessEqual(None, api._get_delta_by_id(delta_id))
+        self.failUnlessEqual(STATUS_NOT_FOUND, api.cancel(delta_id))
+        self.failUnlessEqual(None, api._get_delta_by_id(delta_id))
 if __name__ == '__main__':
     unittest.main()
