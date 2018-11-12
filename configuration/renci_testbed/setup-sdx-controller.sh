3a0ec815cf3d0b34a58fc0be5605ad9a0ef10ec5
mcevik0 <mcevik@renci.org>
2018-11-12 17:39:41 -0500

    Updates for renci_testbed setup

--- configuration/renci_testbed/setup-sdx-controller.sh
+++ configuration/renci_testbed/setup-sdx-controller.sh
@@ -8,18 +8,18 @@ sudo yum -y install git docker.io
 sudo usermod -aG docker $USER
 
 # git work
-git clone https://github.com/RENCI-NRIG/atlanticwave-sdx/atlanticwave-proto.git
-git checkout renci_testbed_setup
+git clone https://github.com/RENCI-NRIG/atlanticwave-proto.git
 
 # Docker work: build SDX Controller container
 cd ~/atlanticwave-proto/
+git checkout renci_testbed_setup
 cp configuration/renci_testbed/renci_ben.manifest docker/sdx_container/
 
 sudo systemctl restart docker
 
 cd docker/sdx_container
 sudo docker build -t sdx_container .
-rm renci_ben.manifest 
+rm -f renci_ben.manifest 
 
 # Copy over run scripts
 cd ~/atlanticwave-proto/configuration/renci_testbed
