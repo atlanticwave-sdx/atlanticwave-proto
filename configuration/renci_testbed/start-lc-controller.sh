7ef9d7dbc4441b3f4eb443077b3872c7fd7fdbb3
mcevik0 <mcevik@renci.org>
2018-11-12 23:35:30 -0500

    Updates for lc and sdx controllers

--- configuration/renci_testbed/start-lc-controller.sh
+++ configuration/renci_testbed/start-lc-controller.sh
@@ -1,9 +1,11 @@
 #/bin/bash
 
 LC_SITE="lc-$1"
+export LC_SITE
 
 # Local Controller
 cd atlanticwave-proto/localctlr/
+docker stop ${LC_SITE}
 docker rm ${LC_SITE}
 export SDXIPVAL="10.14.11.254"
 docker run -e MANIFEST="/renci_ben.manifest" -e SITE="${LC_SITE}" -e SDXIP=${SDXIPVAL} -p 6680:6680 -dit --name=${LC_SITE} lc_container
--- configuration/renci_testbed/start-sdx-controller.sh
+++ configuration/renci_testbed/start-sdx-controller.sh
@@ -2,6 +2,7 @@
 
 # SDX Controller
 cd atlanticwave-proto/sdxctlr/
+docker stop sdxrencitestbed
 docker rm sdxrencitestbed
 docker run -e MANIFEST="/renci_ben.manifest" -e IPADDR="0.0.0.0" -e PORT="5000" -e LCPORT="5555" -p 5000:5000 -dit --name=sdxrencitestbed sdx_container
 
