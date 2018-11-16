d050aab2e1c43f1c59d2659e817c652c55f826f4
mcevik0 <mcevik@renci.org>
2018-11-16 12:35:11 -0500

    Change SDX controller IP address in start script

--- configuration/renci_testbed/start-lc-controller.sh
+++ configuration/renci_testbed/start-lc-controller.sh
@@ -31,7 +31,8 @@ esac
 cd atlanticwave-proto/localctlr/
 docker stop ${LC_SITE}
 docker rm ${LC_SITE}
-export SDXIPVAL="10.14.11.254"
+export SDXIPVAL="10.14.12.254"
+
 #docker run -e MANIFEST="/renci_ben.manifest" -e SITE="${LC_SITE}" -e SDXIP=${SDXIPVAL} -p 6680:6680 -dit --name=${LC_SITE} lc_container
 docker run --rm --network host -e MANIFEST="/renci_ben.manifest" -e SITE="${LC_SITE}" -e SDXIP=${SDXIPVAL} -p ${RYU_PORT}:${RYU_PORT} -${OPTS} --name=${LC_SITE} lc_container
 
