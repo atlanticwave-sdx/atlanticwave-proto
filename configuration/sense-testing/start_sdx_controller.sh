0dde9b6437a1b3a82d1c5dcbec482e31f80c2954
Sean Donovan <sdonovan@gatech.edu>
2019-04-04 11:06:34 -0400

    #92 - startup scripts to make it easier to test.

---
+++ configuration/sense-testing/start_sdx_controller.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Variables used for nearly everything
+source variables.sh
+
+# stop existing SDX container
+docker stop -name=$SDX_CONTAINTER_NAME
+
+# Start new SDX container
+docker run -e MANIFEST=$MANIFEST_FILE -e IPADDR=$SDX_IPADDR -e PORT=$SDX_PORT -e LCPORT=$SDX_LCPORT -e PYTHONPATH=$PYTHON_PATH -e AWAVEDIR=$CONTAINER_AWAVE_DIRECTORY -p $SDX_PORT:$SDX_PORT -v $SDX_VOLUMES -it -name=$SDX_CONTAINER_NAME sdx_container $SDX_START_COMMAND
+#FIXME: outbound ports for APIs?
