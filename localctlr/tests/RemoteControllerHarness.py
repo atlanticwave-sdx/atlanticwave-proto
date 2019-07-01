241b6f18a6207d7db91d4fbf1a7475d0c3c8ef2b
Sean Donovan <sdonovan@gatech.edu>
2019-07-01 13:11:10 -0400

    #113 - Fixing string formating.

--- localctlr/tests/RemoteControllerHarness.py
+++ localctlr/tests/RemoteControllerHarness.py
@@ -25,55 +25,55 @@ class RemoteControllerHarness(object):
         # Create useful examples
         self.examples = []
         self.examples.append(SDXIngressPolicy('dummy',
-            '{"SDXIngress":{
-            "starttime":"1985-04-12T23:20:50",
-            "endtime":"2085-04-12T23:20:50",
-            "switch":"atl-switch",
-            "matches":[DSTMAC("00:00:00:00:00:00")],
-            "actions":[ModifyDSTMAC("00:00:00:00:00:02")]}
-            }'))
+            ('{"SDXIngress":{'
+             '"starttime":"1985-04-12T23:20:50",'
+             '"endtime":"2085-04-12T23:20:50",'
+             '"switch":"atl-switch",'
+             '"matches":[DSTMAC("00:00:00:00:00:00")],'
+             '"actions":[ModifyDSTMAC("00:00:00:00:00:02")]}}'
+            )))
                              
         #self.examples.append(OFR(OpenFlowMatch([ETH_DST("00:00:00:00:00:01")]),
         #                         instruction_APPLY_ACTIONS([action_SET_FIELD(ETH_DST("00:00:00:00:00:02"))])))
 
         self.examples.append(SDXIngressPolicy('dummy',
-            '{"SDXEgress":{
-            "starttime":"1985-04-12T23:20:50",
-            "endtime":"2085-04-12T23:20:50",
-            "switch":"atl-switch",
-            "matches":[ETHTYPE(0x0800), SRCIP("1.2.3.4")],
-            "actions":[ModifySRCIP("2.3.4.5")]}
-            }'))
+            ('{"SDXEgress":{'
+             '"starttime":"1985-04-12T23:20:50",'
+             '"endtime":"2085-04-12T23:20:50",'
+             '"switch":"atl-switch",'
+             '"matches":[ETHTYPE(0x0800), SRCIP("1.2.3.4")],'
+             '"actions":[ModifySRCIP("2.3.4.5")]}}'
+            )))
         #self.examples.append(OFR(OpenFlowMatch([ETH_TYPE(0x0800), 
         #                                        IPV4_SRC("1.2.3.4")]),
         #                         instruction_APPLY_ACTIONS([action_SET_FIELD(IPV4_DST("2.3.4.5"))])))
 
         self.examples.append(SDXIngressPolicy('dummy',
-            '{"SDXEgress":{
-            "starttime":"1985-04-12T23:20:50",
-            "endtime":"2085-04-12T23:20:50",
-            "switch":"atl-switch",
-            "matches":[DSTMAC("00:00:00:00:00:03"),
-                       ETHTYPE(0x0800), 
-                       SRCIP("3.4.5.6")],
-            "actions":[ModifySRCMAC("00:00:00:00:00:04")]}
-            }'))
+            ('{"SDXEgress":{'
+             '"starttime":"1985-04-12T23:20:50",'
+             '"endtime":"2085-04-12T23:20:50",'
+             '"switch":"atl-switch",'
+             '"matches":[DSTMAC("00:00:00:00:00:03"),' 
+             '                  ETHTYPE(0x0800),'
+             '          SRCIP("3.4.5.6")],'
+             '"actions":[ModifySRCMAC("00:00:00:00:00:04")]}}'
+            )))
         #self.examples.append(OFR(OpenFlowMatch([ETH_DST("00:00:00:00:00:03"), 
         #                                        ETH_TYPE(0x0800), 
         #                                        IPV4_SRC("3.4.5.6")]),
         #                         instruction_APPLY_ACTIONS([action_SET_FIELD(ETH_SRC("00:00:00:00:00:04"))])))
 
         self.examples.append(SDXIngressPolicy('dummy',
-            '{"SDXEgress":{
-            "starttime":"1985-04-12T23:20:50",
-            "endtime":"2085-04-12T23:20:50",
-            "switch":"atl-switch",
-            "matches":[DSTMAC("00:00:00:00:00:05"),
-                       ETHTYPE(0x0800), 
-                       SRCIP("4.5.6.7")],
-            "actions":[ModifySRCMAC("00:00:00:00:00:04"),
-                       DSTIP("5.6.7.8")]}
-            }'))
+            '{"SDXEgress":{'
+            '"starttime":"1985-04-12T23:20:50",'
+            '"endtime":"2085-04-12T23:20:50",'
+            '"switch":"atl-switch",'
+            '"matches":[DSTMAC("00:00:00:00:00:05"),'
+            '           ETHTYPE(0x0800), '
+            '           SRCIP("4.5.6.7")],'
+            '"actions":[ModifySRCMAC("00:00:00:00:00:04"),'
+            '           DSTIP("5.6.7.8")]}}'
+            )))
         #self.examples.append(OFR(OpenFlowMatch([ETH_DST("00:00:00:00:00:05"),
         #                                        ETH_TYPE(0x0800),
         #                                        IPV4_SRC("4.5.6.7")]),
@@ -81,14 +81,14 @@ class RemoteControllerHarness(object):
         #                                                    action_SET_FIELD(IPV4_DST("5.6.7.8"))])))
         
         self.examples.append(SDXIngressPolicy('dummy',
-            '{"SDXEgress":{
-            "starttime":"1985-04-12T23:20:50",
-            "endtime":"2085-04-12T23:20:50",
-            "switch":"atl-switch",
-            "matches":[IPPROTO(6),
-                       ETHTYPE(0x0800)],
-            "actions":[Forward1)]}
-            }'))
+            ('{"SDXEgress":{'
+             '"starttime":"1985-04-12T23:20:50",'
+             '"endtime":"2085-04-12T23:20:50",'
+             '"switch":"atl-switch",'
+             '"matches":[IPPROTO(6),'
+             '           ETHTYPE(0x0800)],'
+             '"actions":[Forward(1)]}}'
+            )))
         #self.examples.append(OFR(OpenFlowMatch([IP_PROTO(6),
         #                                        ETH_TYPE(0x0800)]),
 #                                 instruction_WRITE_ACTIONS([action_OUTPUT(1)]),
