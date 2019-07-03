fa3140363e1692205f2061e27bf109fd1e6fee7b
Sean Donovan <sdonovan@gatech.edu>
2019-07-03 12:44:42 -0400

    #112 - Timing incorrect?

--- shared/tests/test_SDXControllerConnectionManagerConnection.py
+++ shared/tests/test_SDXControllerConnectionManagerConnection.py
@@ -183,9 +183,9 @@ class SDXConnectionEstablishmentTest(unittest.TestCase):
 
         self.ServerCxn = SDXControllerConnection(self.ip, self.port,
                                                         sock)
+        sleep(.5) # Waiting for connection.
         self.ServerCxn.set_new_callback(new_callback)
         self.ServerCxn.set_delete_callback(del_callback)
-        sleep(.5) # Waiting for connection.
         self.ServerCxn.transition_to_main_phase_SDX(set_name_1,
                                                     get_initial_rules_5)
         
@@ -305,9 +305,9 @@ class SDXConnectionHeartbeatTest(unittest.TestCase):
 
         self.ServerCxn = SDXControllerConnection(self.ip, self.port,
                                                         sock)
+        sleep(.5) # Wait for connection.
         self.ServerCxn.set_new_callback(new_callback)
         self.ServerCxn.set_delete_callback(del_callback)
-        sleep(.5) # Wait for connection.
         self.ServerCxn.transition_to_main_phase_SDX(set_name_1,
                                                     get_initial_rules_5)
 
