0a05f5e0db7fa9f576253c7bd9368bf192050e22
Sean Donovan <sdonovan1985@users.noreply.github.com>
2019-09-23 15:36:50 -0400

    #100 - Updating variables for MIA.

--- configuration/awave-deployment/mia/variables.sh
+++ configuration/awave-deployment/mia/variables.sh
@@ -2,7 +2,7 @@
 
 # The xxx_START_COMMAND variables can be empty.
 
-LOCAL_AWAVE_DIRECTORY="~/atlanticwave-proto"
+LOCAL_AWAVE_DIRECTORY="/home/sean/atlanticwave-proto"
 CONTAINER_AWAVE_DIRECTORY="/development"
 MANIFEST_FILE="/development/configuration/awave-deployment/awave.manifest"
 PYTHON_PATH=".:/development/"
@@ -11,8 +11,8 @@ LC_CONTAINER_NAME="mia_local_controller"
 LC_VOLUMES="$LOCAL_AWAVE_DIRECTORY:$CONTAINER_AWAVE_DIRECTORY:rw"
 #LC_IP_ADDR="192.168.1.27"
 LC_IP_ADDR="10.100.1.27"
-LC_PORT="6653"
-LC_SITE="mia"
+LC_PORT="6633"
+LC_SITE="mia-ctlr-186.106"
 #LC_SDXIP="192.168.1.21"
 LC_SDXIP="10.100.1.21"
 LC_START_COMMAND=""
