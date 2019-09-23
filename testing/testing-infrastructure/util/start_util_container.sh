9b3b9394f0938f18cb6bb85c42af1b53bdec66a1
Sean Donovan <sdonovan1985@users.noreply.github.com>
2019-09-23 13:40:30 -0400

    #100 - Fix for starting util_container.

--- testing/testing-infrastructure/util/start_util_container.sh
+++ testing/testing-infrastructure/util/start_util_container.sh
@@ -5,4 +5,4 @@ CTLR_PORT=6633
 
 docker stop $CONTAINER_NAME
 
-docker run -p $CTLR_PORT:$CTLR_PORT --it --name=$CONTAINER_NAME util_container
+docker run -p $CTLR_PORT:$CTLR_PORT -it --name=$CONTAINER_NAME util_container
