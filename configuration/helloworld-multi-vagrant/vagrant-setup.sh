e35c5d0324d5fa76333121afa9a93a758f19620e
Yufeng Xin <yxin@renci.org>
2020-07-02 12:33:04 -0400

    change the vagrant related configuration to use this branch to build the emulation

--- configuration/helloworld-multi-vagrant/vagrant-setup.sh
+++ configuration/helloworld-multi-vagrant/vagrant-setup.sh
@@ -9,7 +9,7 @@ sudo usermod -aG docker $USER
 
 
 # git work
-git clone -b master https://github.com/atlanticwave-sdx/atlanticwave-proto.git
+git clone -b python3upgrade https://github.com/atlanticwave-sdx/atlanticwave-proto.git
 
 # Docker work: build SDX Controller and Local Controller containers
 cd ~/atlanticwave-proto/
