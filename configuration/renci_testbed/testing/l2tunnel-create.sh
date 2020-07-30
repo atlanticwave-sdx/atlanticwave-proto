29e159c4b786493f45ab5182501aad33a531bb53
mcevik0 <mcevik@renci.org>
2020-07-30 11:03:42 -0400

    Update test script

--- configuration/renci_testbed/testing/l2tunnel-create.sh
+++ configuration/renci_testbed/testing/l2tunnel-create.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#set -x
 #SRC_SW="rencis1"
 #DST_SW="dukes1"
 #SRC_PORT=12
@@ -13,8 +14,14 @@ SRC_VLAN=$3
 DST_VLAN=$4
 SRC_PORT=12
 DST_PORT=12
+BANDWIDTH=800000
 START=`date "+%Y-%m-%dT%H:%M:%S"`
 END=`date --date="3 days" "+%Y-%m-%dT%H:%M:%S"`
+
+SDX_CONTROLLER="atlanticwave-sdx-controller.renci.ben"
+SDX_CONT_PORT=5000
+REST_ENDPOINT="http://${SDX_CONTROLLER}:${SDX_CONT_PORT}/api/v1/policies/type/L2Tunnel"
+COOKIE="cookie-mcevik.txt"
 
 curl \
 -X POST ${REST_ENDPOINT} \
@@ -29,7 +36,7 @@ curl \
       "srcswitch":"${SRC_SW}","dstswitch":"${DST_SW}",
       "srcport":${SRC_PORT},"dstport":${DST_PORT},
       "srcvlan":${SRC_VLAN},"dstvlan":${DST_VLAN},
-      "bandwidth":800000
+      "bandwidth":${BANDWIDTH}
     }
 }
 EOF
