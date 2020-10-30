2d6bff7496ca3ce464f514537055f9cfae582527
Mert Cevik <mcevik@renci.org>
2020-10-30 15:51:01 -0400

    Change management VLAN range to deploy the branch

--- shared/ManagementVLANLCRule.py
+++ shared/ManagementVLANLCRule.py
@@ -28,10 +28,10 @@ from LCRule import *
 
 ###MANAGEMENT_VLAN_MIN = 4080
 ###MANAGEMENT_VLAN_MAX = 4089
-MANAGEMENT_VLAN_MIN = 3001
-MANAGEMENT_VLAN_MAX = 3006
-###MANAGEMENT_VLAN_MIN = 1401
-###MANAGEMENT_VLAN_MAX = 1415
+#MANAGEMENT_VLAN_MIN = 3001
+#MANAGEMENT_VLAN_MAX = 3006
+MANAGEMENT_VLAN_MIN = 1401
+MANAGEMENT_VLAN_MAX = 1415
 class ManagementVLANLCRule(LCRule):
     ''' This structure is used at each switch that the Management VLAN traverses
         to define the physical ports that the VLAN will traverse. Traffic 
