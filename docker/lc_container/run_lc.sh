efbbc1e25d0646b8e412c8cd4464546596206aae
Mert Cevik <mcevik@renci.org>
2021-02-13 02:31:56 -0500

    Remove argument

--- docker/lc_container/run_lc.sh
+++ docker/lc_container/run_lc.sh
@@ -10,10 +10,10 @@ then
   MANIFEST="../demo/aw-three-site.manifest"
 fi
 
-if ! test $SPANNINGTREEMANIFEST
-then 
-  MANIFEST="../demo/aw-three-site_spanning_tree.manifest"
-fi
+#if ! test $SPANNINGTREEMANIFEST
+#then 
+#  MANIFEST="../demo/aw-three-site_spanning_tree.manifest"
+#fi
 
 if ! test $SDXIP
 then 
